2416e42992db1c89c5bff23d562cc5115e8b93a8
Balasai234 <balasaicheni7@gmail.com>
2026-03-11 11:18:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "servers" {
+  default = {
+    server1 = "t3.micro"
+    server2 = "t3.micro"
+    server3 = "t3.micro"
+  }
+}
+
+resource "aws_instance" "web" {
+  for_each = var.servers
+
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = each.value
+
+  tags = {
+    Name = each.key
+  }
+}
